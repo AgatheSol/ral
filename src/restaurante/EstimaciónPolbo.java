b5381c37df1a18be4d66233e26354ed73f44bb26
ytaly <ytaly@192.168.0.10>
2016-11-09 23:15:02 +0100

    comit inicio

--- src/restaurante/EstimaciónPolbo.java
+++ src/restaurante/EstimaciónPolbo.java
@@ -29,7 +29,7 @@ public class EstimaciónPolbo {
     }
 
     public void getClientes() {
-        System.out.println(" A cantidade de clientes que pode recibir o restaurante e "+((polbo+patacas*3)/2);
+        System.out.println(" A cantidade de clientes que pode recibir o restaurante e "+((polbo+patacas*3)/2));
                
     }
        
